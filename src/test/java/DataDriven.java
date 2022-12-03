5a51d859cb998f2311a9a947e4a60cd33df1a3b3
helen <rqhhelen@gmail.com>
2022-12-03 16:37:34 -0500

    first commit

---
+++ src/test/java/DataDriven.java
@@ -0,0 +1,92 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataDriven {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+
+	}
+
+	public ArrayList<String> getData(String testcaseName) throws IOException {
+	
+	ArrayList<String> a = new ArrayList<String>();
+	
+	FileInputStream fis = new FileInputStream("C:\\Users\\hhele\\OneDrive\\Desktop\\Udemy\\DataDriven\\Book1.xlsx");
+
+	XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+	// get the number of sheets in Excel
+	int sheets = workbook.getNumberOfSheets();
+
+	for (int i = 0; i < sheets; i++) {
+		if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
+			XSSFSheet sheet = workbook.getSheetAt(i);
+
+			// identify test cases column by scanning the entire 1st row
+			Iterator<Row> rows = sheet.iterator(); // sheet is a collection of rows
+
+			// get access to first row
+			Row firstrow = rows.next();
+			
+			Iterator<Cell> ce = firstrow.cellIterator(); // ce is collection of cells
+			
+			int k = 0;
+			int column = 0;
+			
+			// look for row that is called Testcases
+			while (ce.hasNext()) {
+				Cell value = ce.next();
+				if (value.getStringCellValue().equalsIgnoreCase("testcases")) {
+					column = k;			
+				}
+				k++;
+			}
+			System.out.println(column);
+			
+			
+			// once column is identified then scan the entire test case column to identify
+			while(rows.hasNext()) {
+				Row r = rows.next();
+				
+				// purchase test case row
+				if(r.getCell(column).getStringCellValue().equalsIgnoreCase(testcaseName)) {
+					// after you grab purchase test case row = pull all the data of that row and feed into test
+					Iterator<Cell> cv = r.cellIterator();
+					
+					while(cv.hasNext()) {
+						
+						//check if the cell is numerical
+						Cell c = cv.next();
+						if(c.getCellTypeEnum()==CellType.STRING) {
+							//adding string cell value to array list
+							a.add(c.getStringCellValue());
+						}else {
+							//adding numerical cell value to array list
+							//first convert number to string then add to array list
+							a.add( NumberToTextConverter.toText(c.getNumericCellValue()));
+							
+						}
+						
+						
+					}
+				}
+			}
+			
+		}
+	}
+	return a;
+	
+	}	
+}
---
+++ src/test/java/Test_Sample.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Test_Sample {
+
+	public static void main(String[] args) throws IOException {
+		
+		DataDriven d = new DataDriven();
+		ArrayList<String> data = d.getData("Add Profile");
+		
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+		
+		//for example using selenium
+		//driver.findElement(By.id("xxx")).sendKeys(data.get(0));
+	}
+
+}
